a4d52ab87c536abb5d082f2cffd76d146976766f
ebina <aclzoppi.0715@gmail.com>
2023-06-01 13:28:49 +0900

    復習問題061

--- fukuv0601/Form1.cs
+++ fukuv0601/Form1.cs
@@ -2,9 +2,32 @@ namespace fukuv0601
 {
     public partial class Form1 : Form
     {
+        int vx = 10;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vx -= 10;
+            MessageBox.Show($"{vx}");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            vx = 10;
+            MessageBox.Show($"{vx}");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
         }
     }
 }
