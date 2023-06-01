dfc02631df4d6f4817a637b0e7d0fd59350aed66
ebina <aclzoppi.0715@gmail.com>
2023-06-01 13:40:56 +0900

    復習061＿２

--- fukuv0601/Form1.cs
+++ fukuv0601/Form1.cs
@@ -10,24 +10,23 @@ namespace fukuv0601
 
         private void label1_Click(object sender, EventArgs e)
         {
-
+            timer1.Enabled = false;
+            label1.Text = "‰Ú–¼";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             vx -= 10;
-            MessageBox.Show($"{vx}");
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             vx = 10;
-            MessageBox.Show($"{vx}");
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1.Left += vx;
+            label1.Left = label1.Left + vx;
         }
     }
 }
